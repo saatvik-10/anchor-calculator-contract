273bd738e7f97fc146227294f7e3cf6c0e67dfb2
saatvik-10 <saatvikmadan529@gmail.com>
2026-06-23 15:13:00 +0530

    initializer

--- programs/calculator/src/instructions/initialize.rs
+++ programs/calculator/src/instructions/initialize.rs
@@ -1,9 +1,17 @@
 use anchor_lang::prelude::*;
 
-#[derive(Accounts)]
-pub struct Initialize {}
+use crate::NewAccount;
 
-pub fn handler(ctx: Context<Initialize>) -> Result<()> {
-    msg!("Greetings from: {:?}", ctx.program_id);
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = signer, space = 8 + 4)]
+    pub new_account: Account<'info, NewAccount>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+pub(crate) fn handler(ctx: Context<Initialize>) -> Result<()> {
+    ctx.accounts.new_account.data = 1;
     Ok(())
 }
