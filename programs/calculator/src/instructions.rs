8a7e4cc2d08a0eb1d98faf6abca0d628b2f59508
saatvik-10 <saatvikmadan529@gmail.com>
2026-06-23 15:13:44 +0530

    instructions mod file

--- programs/calculator/src/instructions.rs
+++ programs/calculator/src/instructions.rs
@@ -1,3 +1,11 @@
+pub mod add;
+pub mod double;
+pub mod halve;
 pub mod initialize;
+pub mod subtract;
 
+pub use add::*;
+pub use double::*;
+pub use halve::*;
 pub use initialize::*;
+pub use subtract::*;
